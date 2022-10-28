5b2bbe533d45a065caa796198adebc0de0777cc2
Vikram Sungadi <55626981+Vikramsungadi@users.noreply.github.com>
2022-10-29 00:05:38 +0530

    Completed Hero Section, Skills section, Projects Section.
    Switched to SCSS
    
    Added Lot more animations

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const menu = document.querySelector(".menu");
-const close = document.querySelector(".fa-xmark");
+const closeBtn = document.querySelector(".fa-xmark");
 const menuLinks = document.querySelector(".menu--links");
 const all = document.querySelector(".portfolio");
+const expand = document.querySelector(".projects");
+const logos = document.querySelectorAll(".skill img");
+const skill_link = document.querySelector("#skill__link");
+const skill_btn = document.querySelector("#skill__btn");
+console.log(skill_btn);
 
 //                                      EVENT LISTENERS
 
 menu.addEventListener("click", menuslide);
-close.addEventListener("click", closeMenu);
+menuLinks.addEventListener("click", closeMenu);
+expand.addEventListener("click", expandDetails);
+skill_link.addEventListener("click", poplogos);
+skill_btn.addEventListener("click", poplogos);
 
 function menuslide(e) {
   if (e.target.classList.contains("fa-bars")) {
@@ -20,7 +28,12 @@ function menuslide(e) {
 }
 
 function closeMenu(e) {
-  if (e.target.classList.contains("fa-xmark")) {
+  parent = e.target.parentElement;
+  if (
+    e.target.classList.contains("fa-xmark") ||
+    parent.classList.contains("menu__btns") ||
+    parent.classList.contains("menu__ul")
+  ) {
     menuLinks.style.animation = "slideout 0.3s ease";
     all.classList.toggle("overlay");
     menu.style.display = "inline-block";
@@ -29,4 +42,78 @@ function closeMenu(e) {
       menuLinks.style.animation = "";
     }, 280);
   }
+}
+
+function expandDetails(e) {
+  console.log(e.target);
+  if (e.target.classList.contains("fa-circle-chevron-down")) {
+    chevron = e.target;
+    projectDetails = e.target.closest(".project__details");
+    console.log(projectDetails);
+    ul = document.createElement("ul");
+    li1 = document.createElement("li");
+    li2 = document.createElement("li");
+    li3 = document.createElement("li");
+
+    checkul = projectDetails.querySelector("ul");
+    if (!checkul) {
+      chevron.style.rotate = "180deg";
+      title = projectDetails.querySelector(".project__title");
+
+      if (title.innerText === "todo1" || "todo2" || "todo3") {
+        li1.innerText = "Editing";
+        li2.innerText = "Filtering";
+        li3.innerText = "DarkMode";
+        ul.append(li1, li2, li3);
+        ul.style.animation = "fadename 450ms ease";
+
+        projectDetails.append(ul);
+
+        setTimeout(() => {
+          ul.style.animation = "";
+        }, 500);
+      }
+    } else {
+      chevron.style.rotate = "0deg";
+
+      checkul.style.animation = "fadename 450ms ease";
+
+      setTimeout(() => {
+        checkul.remove();
+      }, 500);
+    }
+  }
+}
+function poplogos(e) {
+  setTimeout(() => {
+    logos[0].style.animation = "popup 1s ease-out";
+
+    setTimeout(() => {
+      logos[1].style.animation = "popup 1s ease-out";
+      setTimeout(() => {
+        logos[2].style.animation = "popup 1s ease-out";
+        setTimeout(() => {
+          logos[3].style.animation = "popup 1s ease-out";
+          setTimeout(() => {
+            logos[4].style.animation = "popup 1s ease-out";
+            setTimeout(() => {
+              logos[5].style.animation = "popup 1s ease-out";
+              setTimeout(() => {
+                logos[6].style.animation = "popup 1s ease-out";
+                setTimeout(() => {
+                  logos[7].style.animation = "popup 1s ease-out";
+
+                  setTimeout(() => {
+                    logos.forEach((logo) => {
+                      logo.style.animation = "";
+                    });
+                  }, 1000);
+                }, 100);
+              }, 125);
+            }, 150);
+          }, 185);
+        }, 175);
+      }, 150);
+    }, 100);
+  }, 100);
 }
